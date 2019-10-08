9ec1d840b2eaf937ac99791eef0691bc3c1509a5
Kefeng Deng <deng@51any.com>
2019-10-08 23:07:13 +1300

    fixed test

--- netty-demos/netty-swagger-demo/netty-swagger-client-demo/src/main/java/nz/net/osnz/java/Application.java
+++ netty-demos/netty-swagger-demo/netty-swagger-client-demo/src/main/java/nz/net/osnz/java/Application.java
@@ -5,12 +5,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 /**
  * @author Kefeng Deng (deng@51any.com)
  */
 @SpringBootApplication
 @RestController
+@EnableWebMvc
 public class Application {
 
     public static void main(String[] args) {
