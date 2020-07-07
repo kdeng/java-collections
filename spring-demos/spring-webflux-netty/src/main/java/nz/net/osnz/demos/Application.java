50080d15fc72c3d6062db2b0c7031bcf25dc2a4a
Kefeng Deng <deng@51any.com>
2020-07-07 22:17:07 +1200

    updated

--- spring-demos/spring-webflux-netty/src/main/java/nz/net/osnz/demos/Application.java
+++ spring-demos/spring-webflux-netty/src/main/java/nz/net/osnz/demos/Application.java
@@ -10,27 +10,19 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.config.EnableWebFlux;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.server.RouterFunction;
-import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.util.Arrays;
-import java.util.Map;
 
-import static org.springframework.http.MediaType.APPLICATION_JSON;
-import static org.springframework.http.MediaType.TEXT_HTML;
-import static org.springframework.http.MediaType.TEXT_PLAIN;
 import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
-import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
-import static org.springframework.web.reactive.function.server.RequestPredicates.contentType;
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 
 /**
@@ -76,18 +68,18 @@ public class Application {
 
   }
 
-  @RestController
-  @RequestMapping("/")
-  @Slf4j
-  public static class SlacKController {
-
-    @PostMapping(path = "/", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity slackPost(@PostForm String body) {
-      log.info(body.toString());
-      return ResponseEntity.ok("ok");
-    }
-
-  }
+//  @RestController
+//  @RequestMapping("/")
+//  @Slf4j
+//  public static class SlacKController {
+//
+//    @PostMapping(path = "/", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+//    public ResponseEntity slackPost(@PostForm String body) {
+//      log.info(body.toString());
+//      return ResponseEntity.ok("ok");
+//    }
+//
+//  }
 
 
   @Bean
